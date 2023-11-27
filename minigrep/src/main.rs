5f2d5b7d35cc9deac212162b874195cf5842794c
HosakaKeigo <gomagoma313@gmail.com>
2023-11-27 13:26:58 +0900

    12.3

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,5 @@
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,34 +12,8 @@ fn main() {
     println!("In file {}", config.file_path);
 
     // read file
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         std::process::exit(1);
     };
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
